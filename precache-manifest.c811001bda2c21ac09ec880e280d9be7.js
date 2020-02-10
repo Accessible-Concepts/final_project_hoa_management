949a5c3acedbfc430080c1ae86ff2dc074c37f8c
Lior Hasson <liorhasson@gmail.com>
2020-02-10 09:07:32 +0200

    Updates

---
+++ precache-manifest.c811001bda2c21ac09ec880e280d9be7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326210f932c2b28d526c89a6d5a8c068",
+    "url": "/liorhasson19/final_project_hoa_management/index.html"
+  },
+  {
+    "revision": "77d521db23a35eff5c17",
+    "url": "/liorhasson19/final_project_hoa_management/static/css/2.a295cc11.chunk.css"
+  },
+  {
+    "revision": "8e58ca0214731f5fd6f8",
+    "url": "/liorhasson19/final_project_hoa_management/static/css/main.dc5af2aa.chunk.css"
+  },
+  {
+    "revision": "77d521db23a35eff5c17",
+    "url": "/liorhasson19/final_project_hoa_management/static/js/2.d7f14a08.chunk.js"
+  },
+  {
+    "revision": "506deff998d706a8e9cba0e1f7c61775",
+    "url": "/liorhasson19/final_project_hoa_management/static/js/2.d7f14a08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e58ca0214731f5fd6f8",
+    "url": "/liorhasson19/final_project_hoa_management/static/js/main.1f8c51f3.chunk.js"
+  },
+  {
+    "revision": "7808726c34e8545090eb",
+    "url": "/liorhasson19/final_project_hoa_management/static/js/runtime-main.65da070a.js"
+  },
+  {
+    "revision": "75197870315e8893e4f0119cd8a4fee8",
+    "url": "/liorhasson19/final_project_hoa_management/static/media/3.75197870.jpg"
+  },
+  {
+    "revision": "bfa56361c9d942d8fa034837615972b2",
+    "url": "/liorhasson19/final_project_hoa_management/static/media/info.bfa56361.png"
+  }
+]);
